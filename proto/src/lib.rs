6cdb0d5bf83e30a075696dfbe6a4b6e4b9ba9669
Rinat <hicc0rus@gmail.com>
2024-07-13 16:22:33 +0500

    added ProtoTransactionCoreInfo

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,19 +1,4 @@
 pub mod zilliqa_proto {
     include!("zilliqa_message.rs");
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_structure() {
-        let test = zilliqa_proto::pm_hello::Data {
-            pubkey: None,
-            listenport: 0,
-        };
-
-        assert_eq!(test.pubkey, None);
-        assert_eq!(test.listenport, 0);
-    }
-}
+pub mod zq1_proto;
