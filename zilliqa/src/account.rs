2b510bbdf870ed6b9fa0a0b28a5ba9ba36a2609c
Rinat <hicc0rus@gmail.com>
2024-08-04 11:34:36 +0500

    added Display impl for accounts

--- zilliqa/src/account.rs
+++ zilliqa/src/account.rs
@@ -15,4 +15,19 @@ impl Account {
 
         Ok(Self { key_pair, address })
     }
+
+    pub fn from_key_pair<'a>(key_pair: KeyPair) -> Result<Self, ZilliqaErrors<'a>> {
+        let address = Address::from_zil_pub_key(&key_pair.pub_key)?;
+
+        Ok(Self { key_pair, address })
+    }
+}
+
+impl std::fmt::Display for Account {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let key_pair = self.key_pair.to_string();
+        let hex_address = hex::encode(self.address.as_slice());
+
+        write!(f, "{}:{}", key_pair, hex_address)
+    }
 }
