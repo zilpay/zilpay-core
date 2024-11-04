07571456a00ab798ca7c0b50e811ed7e736ea3aa
Rinat <rinat@Rinats-MacBook-Air.local>
2024-11-04 20:02:58 +0900

    added auth method for wallet

--- wallet/src/wallet_data.rs
+++ wallet/src/wallet_data.rs
@@ -2,7 +2,7 @@ use crate::{account::Account, wallet_types::WalletTypes};
 use serde::{Deserialize, Serialize};
 use settings::wallet_settings::WalletSettings;
 
-#[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq, Clone, Copy)]
 pub enum AuthMethod {
     FaceId,
     Fingerprint,
@@ -10,6 +10,30 @@ pub enum AuthMethod {
     PinCode,
     #[default]
     None,
+}
+
+impl From<AuthMethod> for String {
+    fn from(method: AuthMethod) -> Self {
+        match method {
+            AuthMethod::FaceId => "faceId".to_string(),
+            AuthMethod::Fingerprint => "fingerprint".to_string(),
+            AuthMethod::Biometric => "biometric".to_string(),
+            AuthMethod::PinCode => "pinCode".to_string(),
+            AuthMethod::None => "none".to_string(),
+        }
+    }
+}
+
+impl From<String> for AuthMethod {
+    fn from(s: String) -> Self {
+        match s.as_str() {
+            "faceId" => AuthMethod::FaceId,
+            "fingerprint" => AuthMethod::Fingerprint,
+            "biometric" => AuthMethod::Biometric,
+            "pinCode" => AuthMethod::PinCode,
+            _ => AuthMethod::None,
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
