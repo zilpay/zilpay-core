52fc107bd2f60d74328f3bc3469704e0352e0b5f
Rinat <rinat@Rinats-MacBook-Air.local>
2024-12-27 21:28:18 +0900

    fixed network trait

--- wallet/src/wallet_network.rs
+++ wallet/src/wallet_network.rs
@@ -1,15 +1,12 @@
-use network::provider::NetworkProvider;
+use rpc::network_config::NetworkConfig;
 
 /// Network management operations
 pub trait NetworkOperations {
     type Error;
 
     /// Adds a new network provider
-    fn add_provider(&mut self, provider: NetworkProvider) -> Result<(), Self::Error>;
+    fn add_provider(&mut self, provider: NetworkConfig) -> Result<(), Self::Error>;
 
     /// Removes an existing network provider
-    fn remove_provider(&mut self, provider: &NetworkProvider) -> Result<(), Self::Error>;
-
-    /// Sets the active network provider
-    fn set_active_provider(&mut self, provider: NetworkProvider) -> Result<(), Self::Error>;
+    fn remove_provider(&mut self, id: u64) -> Result<(), Self::Error>;
 }
