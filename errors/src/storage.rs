f718d0059c30fa4c10c0db6a660e8e236f3424bd
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-09 14:25:45 +0900

    fixed errors

--- errors/src/storage.rs
+++ errors/src/storage.rs
@@ -4,28 +4,40 @@ use thiserror::Error;
 pub enum LocalStorageError {
     #[error("Storage path error")]
     StoragePathError,
+
     #[error("Storage access error: {0}")]
     StorageAccessError(String),
+
     #[error("Failed to load bytes tree")]
     FailToloadBytesTree,
+
     #[error("Failed to create file")]
     FailToCreateFile,
+
     #[error("Failed to write file")]
     FailToWriteFile,
+
     #[error("Storage data not found")]
     StorageDataNotFound,
+
     #[error("Storage write error: {0}")]
     StorageWriteError(String),
+
     #[error("Storage time went backwards")]
     StorageTimeWentBackwards,
+
     #[error("Payload version parse error")]
     PayloadVersionParseError,
+
     #[error("Payload parse error")]
     PayloadParseError,
+
     #[error("Insufficient bytes")]
     InsufficientBytes,
+
     #[error("Payload length error")]
     PayloadLengthError,
+
     #[error("Invalid bytes size overflow")]
     InvalidBytesSizeOverflow,
 }
