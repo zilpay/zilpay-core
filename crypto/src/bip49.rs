2b7524f804437fc613323e70944c1f6ba94fc46a
Rinat <you@example.com>
2025-09-07 13:07:24 +0500

    added components_to_derivation_path

--- crypto/src/bip49.rs
+++ crypto/src/bip49.rs
@@ -35,6 +35,18 @@ pub fn split_path(path: &str) -> Result<Vec<u32>, Bip329Errors> {
         .collect()
 }
 
+pub fn components_to_derivation_path(components: &[u32]) -> Vec<u8> {
+    let mut buffer = Vec::with_capacity(1 + components.len() * 4);
+
+    buffer.push(components.len() as u8);
+
+    for component in components {
+        buffer.extend_from_slice(&component.to_be_bytes());
+    }
+
+    buffer
+}
+
 impl DerivationPath {
     pub fn new(slip44: u32, index: usize) -> Self {
         Self { slip44, index }
@@ -134,6 +146,51 @@ mod tests {
         assert_eq!(
             err,
             Bip329Errors::InvalidComponent("4294967296".to_string())
+        );
+    }
+
+    #[test]
+    fn test_components_to_derivation_path() {
+        let components_eth = vec![
+            44u32.wrapping_add(0x80000000), // 44'
+            60u32.wrapping_add(0x80000000), // 60'
+            0u32.wrapping_add(0x80000000),  // 0'
+            0,
+            0,
+        ];
+        let buffer_eth = components_to_derivation_path(&components_eth);
+        let expected_hex_eth = "058000002c8000003c800000000000000000000000";
+        assert_eq!(
+            hex::encode(buffer_eth),
+            expected_hex_eth,
+            "Standard ETH path failed"
+        );
+
+        let components_empty: Vec<u32> = vec![];
+        let buffer_empty = components_to_derivation_path(&components_empty);
+        let expected_hex_empty = "00";
+        assert_eq!(
+            hex::encode(buffer_empty),
+            expected_hex_empty,
+            "Empty path failed"
+        );
+
+        let components_non_hardened = vec![0, 1, 2];
+        let buffer_non_hardened = components_to_derivation_path(&components_non_hardened);
+        let expected_hex_non_hardened = "03000000000000000100000002";
+        assert_eq!(
+            hex::encode(buffer_non_hardened),
+            expected_hex_non_hardened,
+            "Non-hardened path failed"
+        );
+
+        let components_single_hardened = vec![123u32.wrapping_add(0x80000000)];
+        let buffer_single_hardened = components_to_derivation_path(&components_single_hardened);
+        let expected_hex_single_hardened = "018000007b";
+        assert_eq!(
+            hex::encode(buffer_single_hardened),
+            expected_hex_single_hardened,
+            "Single hardened path failed"
         );
     }
 }
